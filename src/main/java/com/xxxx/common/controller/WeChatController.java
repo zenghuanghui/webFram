ebaedc3816fa9c93130810b983a15dfbdca70d1b
zhh <371286948@qq.com>
2019-09-16 23:06:14 +0800

    添加微信接口

---
+++ src/main/java/com/xxxx/common/controller/WeChatController.java
@@ -0,0 +1,43 @@
+package com.xxxx.common.controller;
+
+
+import com.xxxx.common.util.WeChatUtil;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@RestController
+@RequestMapping("/weChat")
+public class WeChatController {
+
+    @GetMapping()
+    public void validToken(HttpServletRequest request, HttpServletResponse response){
+        // 微信加密签名，signature结合了开发者填写的token参数和请求中的timestamp参数、nonce参数。
+        String signature = request.getParameter("signature");
+        // 时间戳
+        String timestamp = request.getParameter("timestamp");
+        // 随机数
+        String nonce = request.getParameter("nonce");
+        // 随机字符串
+        String echostr = request.getParameter("echostr");
+
+        PrintWriter out = null;
+        try {
+            out = response.getWriter();
+            // 通过检验signature对请求进行校验，若校验成功则原样返回echostr，否则接入失败
+            if (WeChatUtil.checkSignature(signature, timestamp, nonce)) {
+                out.print(echostr);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            out.close();
+            out = null;
+        }
+    }
+}
--- src/main/java/com/xxxx/mobile/bean/intelligentanswer/dto/QuestionDto.java
+++ src/main/java/com/xxxx/mobile/bean/intelligentanswer/dto/QuestionDto.java
@@ -8,9 +8,16 @@ import javax.validation.constraints.NotEmpty;
  * @author zenghuanghui（20260606）
  * @since 2019/9/14 10:06
  */
-@Data
 public class QuestionDto {
 
     @NotEmpty
     private String question;
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
 }
